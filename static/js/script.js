a18becebab3b7bef9c385b95ae6a846008503987
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-23 15:03:59 +0300

    query selector for loanform result summary and sammaryList

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+const loanForm = document.getElementById('loanForm');
+const result = document.getElementById('result');
+const summary = document.getElementById('summary');
+const summaryList = document.getElementById('summaryList');
+
